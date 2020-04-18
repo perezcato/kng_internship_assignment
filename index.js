2f6eaaf90bf19df246b7864deab047ec2be1b71f
Perez Cato <perezcatoc@gmail.com>
2020-04-18 17:07:43 +0000

    solved all the simple errors

--- index.js
+++ index.js
@@ -7,20 +7,17 @@ const app = express();
 const upload =  multer();
 
 app.set('view engine', 'ejs');
+app.get('/',(req,res) => {
+   res.send('KNG Assignment One');
+});
 
 app.post('/', upload.any(), async (req, res) => {
-   const file = req.files[0];
-   const writeStream = fs.createWriteStream('./upload.json');
-   writeStream.write(file.buffer,'utf8');
-   const userData = await fsExtra.readJson('./upload.json');
-   console.log(userData);
-   res.render('response', {userData});
+   if(req.files[0].mimetype !== 'application/json') res.status(422).send('Please upload a json file');
+   else{
+      await fs.createWriteStream('./upload.json').write(req.files[0].buffer,'utf8');
+      res.status(200).render('response', { userData: await fsExtra.readJson('./upload.json')});
+   }
 });
 app.listen(3000,() => {
    console.log('server started listening on port 3000');
 });
-
-
-const createHTML = (jsonData) => {
-
-};

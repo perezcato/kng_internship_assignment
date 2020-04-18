0a1fb08f45650d8654db7b1d5b28677fa81e91d9
Perez Cato <perezcatoc@gmail.com>
2020-04-18 22:49:53 +0000

    fixed the file name extension

--- index.js
+++ index.js
@@ -5,18 +5,19 @@ import fsExtra from 'fs-extra';
 
 const app = express();
 const upload =  multer();
-
 app.set('view engine', 'ejs');
 app.get('/',(req,res) => {
+   console.log('server connected');
    res.send('KNG Assignment One');
 });
 
 app.post('/', upload.any(), async (req, res) => {
-   if(req.files[0].mimetype !== 'application/json') res.status(422).send('Please upload a json file');
-   else{
-      await fs.createWriteStream('./upload.json').write(req.files[0].buffer,'utf8');
-      res.status(200).render('response', { userData: await fsExtra.readJson('./upload.json')});
-   }
+      if(req.files[0].fieldname !== 'user_data') res.status(422).send('Invalid field name');
+      else if(req.files[0].mimetype !== 'application/json') res.status(422).send('Please upload a json file');
+      else{
+         await fs.createWriteStream('./upload.json').write(req.files[0].buffer,'utf8');
+         res.status(200).render('response', { userData: await fsExtra.readJson('./upload.json')});
+      }
 });
 app.listen(3000,() => {
    console.log('server started listening on port 3000');

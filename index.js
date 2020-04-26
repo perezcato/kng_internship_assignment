e939b28eec517d09ab3554302f70b0a5eb2b62bd
Perez Cato <perezcatoc@gmail.com>
2020-04-26 18:10:48 +0000

    done adding packages to the first assignment

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 import express from 'express';
 import multer from 'multer';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import path from 'path';
 import fs from 'fs';
 import fsExtra from 'fs-extra';
 
 const app = express();
 const upload =  multer();
+
+app.set('views', path.join(process.cwd(),'views'));
 app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(process.cwd(),'public')));
+app.set(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(session({secret: 'kng_internship'}));
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+
 app.get('/',(req,res) => {
    console.log('server connected');
    res.send('KNG Assignment One');
